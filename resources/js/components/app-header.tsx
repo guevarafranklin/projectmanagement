9c02af3dad0b59d56bc154e9c0ec4ffde4b0b37b
guevarafranklin <tecguevara@gmail.com>
2025-03-04 20:01:10 -0600

    changed header

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -21,19 +21,20 @@ const mainNavItems: NavItem[] = [
         url: '/dashboard',
         icon: LayoutGrid,
     },
-];
-
-const rightNavItems: NavItem[] = [
     {
-        title: 'Repository',
-        url: 'https://github.com/laravel/react-starter-kit',
+        title: 'Projects',
+        url: '/projects',
         icon: Folder,
     },
     {
         title: 'Documentation',
-        url: 'https://laravel.com/docs/starter-kits',
+        url: '/documentation',
         icon: BookOpen,
     },
+];
+
+const rightNavItems: NavItem[] = [
+
 ];
 
 const activeItemStyles = 'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100';
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -8,12 +8,13 @@ const breadcrumbs: BreadcrumbItem[] = [
         title: 'Dashboard',
         href: '/dashboard',
     },
+
 ];
 
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
+            <Head title="Project Management" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
